49704f575941f7b4611c795e62090309930c73b1
Yarin96 <yarin0008@gmail.com>
2023-11-19 15:20:30 +0200

    production changes #1

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-const nextConfig = {
-  //   env: {
-  //     mongodb_username: "tech-blog",
-  //     mongodb_password: "epNNyjTELWndq5I7",
-  //     mongodb_clustername: "cluster0",
-  //     mongodb_database: "tech-blog",
-  //   },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
